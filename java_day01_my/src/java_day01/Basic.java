7106f19111082d8311156b95e6fd02997e4bb781
JungWooLee <wjddn3711@naver.com>
2021-09-04 21:29:51 +0900

    commit javaday1

---
+++ java_day01_my/src/java_day01/Basic.java
@@ -0,0 +1,22 @@
+package java_day01;
+
+public class Basic {
+	public static void main(String[] args) {
+		/*이 코드는 출력을 위한 것입니다. 주석 컴파일시 제외됨 
+		그리고 다음
+		그리고 다음*/
+		
+//		System.out.print("안녕");
+//		System.out.println("Hello World");
+//		System.out.print("안녕");
+//		System.out.println();
+//		System.out.print("안녕");
+
+		//오늘은자바 첫 시간입니다
+		//만나서 반갑습니다
+		System.out.println("오늘은 자바 첫 시간입니다");
+		System.out.println("만나서 반갑습니다.");
+	 
+	}
+
+}
---
+++ java_day01_my/src/java_day01/VariableTask.java
@@ -0,0 +1,15 @@
+package java_day01;
+
+public class VariableTask {
+
+	public static void main(String[] args) {
+//		// 변수 
+//		System.out.println("넘을 출력해줘!");
+//		int num =10;
+//		System.out.println(num + 20);
+		String a = "안녕";
+//		String a; 오류 이미 선언된 변수를 다시 선언하는 것으로 인식
+		System.out.println(a);
+	}
+
+}
